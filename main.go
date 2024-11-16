7265ff2363b241c70bb27c544399467c81e80d1c
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2024-11-16 09:39:14 +0530

    Implement getName function to greet user by name

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 import "fmt"
 
+func getName() {
+	name := "Nivindu"
+	fmt.Printf("Enter your name: ")
+	fmt.Scanf("%s", &name)
+	fmt.Printf("Hello %s!", name)
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	getName()
 }
