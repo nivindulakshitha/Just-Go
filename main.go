24888e964fc3a552ec73221b15ab77471dfa631d
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2024-11-25 20:27:40 +0530

    Refactor getName function to improve user interaction and add print utility

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 import "fmt"
 
-func getName() {
-	name := "Nivindu"
-	fmt.Printf("Enter your name: ")
-	fmt.Scanf("%s", &name)
-	fmt.Printf("Hello %s!", name)
+func print(text string, method string) {
+	switch method {
+		case "same-line":
+			fmt.Print(text)
+			break
+		default:
+			fmt.Println(text)
+	}
+}
+
+func getName() string {
+	name := ""
+
+	print("Welcome to Tim's Casino!", "new-line")
+	print("Enter your name: ", "same-line")
+	_, err := fmt.Scanf("%s", &name)
+
+	if err != nil {
+		return ""
+	}
+
+	print(fmt.Sprintf("Hello, %s!", name), "new-line")
+	return name
 }
 
 func main() {
