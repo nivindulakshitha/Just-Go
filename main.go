f2031c485e1ba2c27aba2632178a75fd1577034c
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2024-11-25 21:23:01 +0530

    Add generateSymbolArray function to create an array from symbol counts

--- main.go
+++ main.go
@@ -45,7 +45,35 @@ func getBet(balance uint) uint {
 	return bet
 }
 
+func generateSymbolArray(symbols map[string]uint) []string {
+	symbolArr := []string{}
+
+	for symbol, count := range symbols {
+		for i := uint(0); i < count; i++ {
+			symbolArr = append(symbolArr, symbol)
+		}
+	}
+
+	return symbolArr
+}
+
 func main() {
+	symbols := map[string]uint{
+		"A": 4,
+		"B": 7,
+		"C": 15,
+		"D": 20,
+	}
+
+	/* multipliers := map[string]uint {
+		"A": 20,
+		"B": 15,
+		"C": 7,
+		"D": 4,
+	} */
+
+	arr := generateSymbolArray(symbols)
+
 	balance := uint(200)
 
 	getName(balance)
